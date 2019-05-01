66ea74664ea4777439a0045a97c9b424f6e6a9d8
Yausuhiro Katayama <yasucom0406@gmail.com>
2019-04-30 23:22:26 -0700

    Added user method

--- services/passport.js
+++ services/passport.js
@@ -13,7 +13,16 @@ passport.use(
             callbackURL: '/auth/google/callback'
         },
         (accessToken, refreshToken, profile, done) => {
-            new User({ googleId: profile.id }).save();
+            User.findOne({ googleId: profile.id })
+                .then(existingUser => {
+                    if(existingUser) {
+                        done(null, existingUser);
+                    } else {
+                        new User({ googleId: profile.id })
+                            .save()
+                            .then(user => done(null, user));
+                    }
+                });
         }
     )
 );
