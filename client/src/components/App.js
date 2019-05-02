40114a31a2462ea9ed40f317e31e4eff1ae606f2
Yausuhiro Katayama <yasucom0406@gmail.com>
2019-05-02 13:08:28 -0700

    Created client

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <a href="/auth/google">Sign In With Google</a>
+        </div>
+    );
+};
+
+export default App;
