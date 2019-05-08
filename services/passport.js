af2dcf859fc85400ff7417d185c28871efb337e5
Yausuhiro Katayama <yasucom0406@gmail.com>
2019-05-08 14:02:38 -0700

    Deleted space

--- services/passport.js
+++ services/passport.js
@@ -29,7 +29,7 @@ passport.use(
             if(existingUser) {
                 return done(null, existingUser);
             }
-            
+
            const user = await new User({ googleId: profile.id }).save();
            done(null, user);
         }
