194a4002903ade4420a41e5d5c874a8bfcda0589
Yausuhiro Katayama <yasucom0406@gmail.com>
2019-05-08 14:02:06 -0700

    Added redirect

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,12 +12,15 @@ module.exports = app => {
         '/auth/google/callback',
         passport.authenticate(
             'google'
-        )
+        ),
+        (req, res) => {
+            res.redirect('/surveys');
+        }
     );
 
     app.get('/api/logout', (req, res) => {
         req.logout();
-        res.send(req.user);
+        res.redirect('/');
     });
 
     app.get('/api/current_user', (req, res) => {
