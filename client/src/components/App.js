f90791082a592dad90dab40f8cc8e550f2326ca7
Yausuhiro Katayama <yasucom0406@gmail.com>
2019-05-08 14:01:04 -0700

    Added react-router-dom

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from "react-redux";
+import * as actions from '../actions';
 
-const App = () => {
-    return (
-        <div>
-            <a href="/auth/google">Sign In With Google</a>
-        </div>
-    );
-};
+import Header from './Header';
+import Landing from './Landing';
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
 
-export default App;
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <Header />
+                    <div className="container">
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/surveys" component={Dashboard}/>
+                        <Route path="/surveys/new" component={SurveyNew}/>
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
+}
+
+export default connect(
+    null,
+    actions
+)(App);
