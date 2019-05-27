3c55568bd08e6d7ef0687e60aa904da2ea270d66
yasuhiro <yasucom4646@gmail.com>
2019-05-27 15:35:48 -0700

    Update jsx

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Dashbord = () => {
     return (
         <div>
-            Dashbord
+            <h1>Dashbord</h1>
+            <div className="fixed-action-btn">
+                <Link to="/surveys/new" className="btn-floating btn-large red">
+                    <i className="material-icons">add</i>
+                </Link>
+            </div>
         </div>
     );
 }
