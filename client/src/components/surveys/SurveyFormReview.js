0dbc3238e3c4c8d1eeeb6f85fd1194a547290081
yasuhiro <yasucom4646@gmail.com>
2019-05-28 09:25:58 -0700

    Create SurveyFormReview.js

---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SurveyReview = () => {
+    return (
+        <div>
+            <h5>Place confirm your entries</h5>
+        </div>
+    );
+};
+
+export default SurveyReview;
