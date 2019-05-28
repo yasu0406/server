b09354fb9ca5700c81a24831042185f859199edd
yasuhiro <yasucom4646@gmail.com>
2019-05-28 09:26:59 -0700

    Changed function to this.props.onSurveySubmit on form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -22,7 +22,7 @@ class SurveyForm extends Component {
     render() {
         return (
             <div>
-                <form onSubmit={this.props.handleSubmit(values => console.log(values))}
+                <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}
                 >
                     {this.renderFields()}
 
