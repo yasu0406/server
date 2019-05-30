10cdc41226731944fbf5b340d3f8dac2d4709a81
yasuhiro <yasucom4646@gmail.com>
2019-05-30 13:50:38 -0700

    Added post for wehooks

--- routes/surveysRoutes.js
+++ routes/surveysRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser').default;
+const { URL } = require('url');
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
@@ -8,7 +11,19 @@ const Survey = mongoose.model("surveys");
 module.exports = app => {
     app.get('/api/surveys/thanks', (req,res) => {
         res.send('Thanks for voting');
-    })
+    });
+
+    app.post('/api/surveys/webhooks', (req, res) => {
+        const events = _.map(req.body, (email, url) => {
+            const pathname = new URL(url).pathname;
+            const p = new Path('/api/surveys/:surveyId/:choice');
+            const match = p.test(pathname);
+            if (match) {
+                return { email, surveyId: match.surveyId, choice: match.choice };
+            }
+        });
+        console.log(events);
+    });
 
     app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients } = req.body;
